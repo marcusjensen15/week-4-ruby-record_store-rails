1d587cbaf1814ca3f0740886a84a824186d42e43
Student <example@example.com>
2020-01-13 14:31:12 -0800

    finish methods for albums controller

---
+++ spec/models/album_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe Album do
+  it { should have_many(:songs) }
+end
+
+# describe Album do
+#   it('has many songs') do
+#     album = Album.create(name: 'album')
+#     song1 = Song.create(name: 'song1', album_id: album.id)
+#     song2 = Song.create(name: 'song2', album_id: album.id)
+#     expect(album.songs()).to(eq([song1, song2]))
+#   end
+# end
